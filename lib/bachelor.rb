1a1b9f48b44cc3141b9a9b95adba5fe8975e35c8
Mike C <mikecronin92@gmail.com>
2018-03-27 06:02:14 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,38 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+require 'pry'
+require 'json'
+
+def b_data 
+  file = File.read('spec/fixtures/contestants.json')
+  return data = JSON.parse(file)
 end
 
-def get_contestant_name(data, occupation)
-  # code here
+def contestants
+  return b_data.map {|k,profile| profile}.flatten
+end
+
+def get_first_name_of_season_winner(data, season)
+  data[season].find {|hash| hash["status"] == "Winner"}["name"].split[0]
+end
+
+def get_contestant_name(data, job)
+  contestants.find {|hash| hash["occupation"] == job}["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestants.map {|val| 1 if val["hometown"] == hometown}.compact.reduce(0, :+) 
+  # remove nil values, then add up all the ones in the array
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestants.find {|val| val["hometown"] == hometown}["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  (data[season].map {|val| val["age"].to_f}.reduce(0, :+)/(data[season].length)).round
+  # ages from maps array added up / by number of indexes, then round
 end
+
+
+# binding.pry
+
+
